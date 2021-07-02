7d91016c3e3272196b63d45ca33be6ce34dc3468
HunainMehar <HunainMehar@github.com>
2021-07-03 01:30:33 +0500

    commit

--- index.js
+++ index.js
@@ -3,3 +3,38 @@ $(document).ready(function () {
     $(".navbar-toggler").toggleClass("change");
   });
 });
+
+// fixed top
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".fixed-top");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
+
+// active links
+$(document).on("click", "ul li", function () {
+  $(this).addClass("active").siblings().removeClass("active");
+});
+
+// back to top
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 20) {
+      $(".back-to-top").fadeIn();
+    } else {
+      $(".back-to-top").fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $(".back-to-top").click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      400
+    );
+    return false;
+  });
+});
